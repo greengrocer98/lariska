7bf7fdbe10451b542033f45f19446ba4dc3a3132
ggrocer <esyunin.maks@mail.ru>
2025-12-27 15:02:11 +0300

    updated paw3395 driver. Added led indicator module

--- local_build.sh
+++ local_build.sh
@@ -9,9 +9,9 @@ set -euxo pipefail
 
 build () {
     local shield=lariska
-    export ZMK_PAW_3395="$HOME/zmk_modules/zmk-paw3395-driver"
-    export ZMK_BEHAVIOR_ATTR_CYCLE="$HOME/zmk_modules/zmk-behavior-sensor-attr-cycle"
-    export ZMK_MODULE_DIRS="${ZMK_PAW_3395};${ZMK_BEHAVIOR_ATTR_CYCLE}"
+    export ZMK_PAW_3395="$HOME/zmk_modules/ggrocer-zmk-paw3395-driver"
+    export ZMK_RGBLED_WIDGET="$HOME/zmk_modules/zmk-vfx-rgbled-indicator"
+    export ZMK_MODULE_DIRS="${ZMK_PAW_3395};${ZMK_RGBLED_WIDGET}"
     rm -rf $CURRENT_DIR/build/$shield
     west build \
         -p -b nice_nano \
@@ -51,7 +51,8 @@ export ZEPHYR_SDK_INSTALL_DIR="$HOME/zephyr-sdk-0.17.0"
 
 pushd $ZMK_APP_DIR
 
-build
+# build
+build_reset
 
 deactivate
 
